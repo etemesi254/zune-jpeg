ea6d6cc2ae07b4ccfe9d6f7722a98791eab5f068
caleb <etemesicaleb@gmail.com>
2021-06-05 22:32:55 +0300

    1. Added Colorspace enum
    2. Added read_u8 function
    
    3. Added read_u16_be function

--- src/misc.rs
+++ src/misc.rs
@@ -1,10 +1,9 @@
 #![allow(dead_code)]
-use byteorder::ReadBytesExt;
-use std::fmt;
-use std::io::Read;
 
-/// Start of image, these are the first two bytes in the Image
-pub const START_OF_IMAGE: u16 = 0xffd8;
+use crate::errors::DecodeErrors;
+use std::fmt;
+use std::io::{BufReader, Read};
+
 /// Start of baseline DCT Huffman coding
 pub const START_OF_FRAME_BASE: u16 = 0xffc0;
 /// Start of another frame
@@ -12,7 +11,7 @@ pub const START_OF_FRAME_EXT_SEQ: u16 = 0xffc1;
 /// Start of progressive DCT encoding
 pub const START_OF_FRAME_PROG_DCT: u16 = 0xffc2;
 
-/// Start of losless sequential Huffman coding
+/// Start of Lossless sequential Huffman coding
 pub const START_OF_FRAME_LOS_SEQ: u16 = 0xffc3;
 /// Start of extended sequential DCT arithmetic coding
 pub const START_OF_FRAME_EXT_AR: u16 = 0xffc9;
@@ -20,28 +19,8 @@ pub const START_OF_FRAME_EXT_AR: u16 = 0xffc9;
 pub const START_OF_FRAME_PROG_DCT_AR: u16 = 0xffca;
 /// Start of Lossless sequential Arithmetic coding
 pub const START_OF_FRAME_LOS_SEQ_AR: u16 = 0xffcb;
-/// Code for Start of app marker
-pub const START_OF_APP_MARKER: u16 = 0xffe0;
-/// 0xffc2-> 65474
-pub const PROGRESSIVE_DCT: u16 = 0xffc2;
-///0xffc4 -> 65476
-pub const HUFFMAN_TABLE: u16 = 0xffc4;
-/// 0xffdb -> 65499
-pub const QUANTIZATION_TABLE: u16 = 0xffdb;
-/// 0xffdd -> 65501
-pub const RESTART_INTERVAL: u16 = 0xffdd;
-/// 0xffda - > 65498
-pub const START_OF_SCAN: u16 = 0xffda;
-/// Signifies end of image
-///
-/// 0xffd9->65497
-pub const END_OF_IMAGE: u16 = 0xffd9;
 
-/// Unzigzag a zig-zagged jpeg
-///
-/// This is used as an index mechanism
-/// Ie calling `UN_ZIGZAG[5]` gives you 2 which means the
-/// Value at index 5 should be moved to index 2
+/// Undo run length encoding of coefficients by placing them in natural order
 #[rustfmt::skip]
 pub const UN_ZIGZAG: [usize; 64] = [
     0,  1,  8,  16, 9,  2,  3, 10,
@@ -54,6 +33,71 @@ pub const UN_ZIGZAG: [usize; 64] = [
     53, 60, 61, 54, 47, 55, 62, 63,
 ];
 
+/// Align data to a 16 byte boundary
+#[repr(align(16))]
+#[derive(Clone)]
+pub struct Aligned16<T: ?Sized>(pub T);
+
+impl<T> Default for Aligned16<T>
+where
+    T: Default,
+{
+    fn default() -> Self {
+        Aligned16(T::default())
+    }
+}
+/// Align data to a 16 byte boundary
+#[repr(align(32))]
+#[derive(Clone)]
+pub struct Aligned32<T: ?Sized>(pub T);
+
+impl<T> Default for Aligned32<T>
+where
+    T: Default,
+{
+    fn default() -> Self {
+        Aligned32(T::default())
+    }
+}
+/// Color conversion types
+///
+/// This enumerates over supported color conversion types the image can decode
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum ColorSpace {
+    /// Red,Green,Blue
+    RGB,
+    /// Monochrome
+    GRAYSCALE,
+    /// YCbCr, (also known as YUV)
+    YCbCr,
+    /// C/M/Y/K
+    CMYK,
+    /// Y/Cb/Cr/K
+    YCCK,
+    /// Blue/Green/Red
+    BGR,
+    /// R,G,B,A colorspace, we set the fourth channel as 255 to represent an
+    /// opaque alpha channel
+    RGBA,
+}
+impl ColorSpace {
+    /// Number of channels (including unused alpha) in this color space
+    pub const fn num_components(self) -> i32 {
+        match self {
+            Self::RGB|Self::BGR| Self::YCbCr => 3,
+            Self::CMYK|Self::RGBA|Self::YCCK => 4,
+            Self::GRAYSCALE => 1,
+        }
+    }
+}
+impl Default for ColorSpace {
+    ///Set default colorspace as RGB
+    ///
+    /// This is the common behaviour for all (sane) Decoder images
+    fn default() -> Self {
+        ColorSpace::RGB
+    }
+}
 /// Markers that identify different Start of Image markers
 /// They identify the type of encoding and whether the file use lossy(DCT) or lossless
 /// compression and whether we use Huffman or arithmetic coding schemes
@@ -132,26 +176,96 @@ impl fmt::Debug for SOFMarkers {
     }
 }
 
-/// Remove FF0 byte snuffed to prevent conditional checking during decoding
-/// Because of such things as byte snuffing
-pub fn remove_ff0<T>(mut buf: T) -> Vec<u8>
+/// Read u8 from a buffer returning the byte
+///
+/// # Arguments
+/// - `reader`: A mutable reference  implementing the `Read` trait
+///
+/// # Returns
+/// The byte read
+/// # Errors
+/// If the reader cannot read the next byte
+///
+/// # Caveats
+/// Some files may be incomplete and for such files, we'll return zeroes as the place holder
+#[inline]
+#[allow(clippy::unused_io_amount)]
+pub fn read_u8<R>(reader: &mut R) -> u8
 where
-    T: Read,
+    R: Read,
 {
-    let mut decoded = Vec::with_capacity(10000);
-    let mut last_value = 0;
-    while let Ok(data) = buf.read_u8() {
-        if !(data == 0x00 && last_value == 0xff) {
-            decoded.push(data)
-        }
-        last_value = data;
-    }
-    let length = decoded.len();
+    let mut tmp = [0; 1];
+    // if there is no more data fill with zero
+    reader
+        .read_exact(&mut tmp)
+        .expect("Could not read from underlying buffer");
 
-    // Remove the last 2 elements that contain the EOI marker
-    unsafe {
-        decoded.set_len(length - 2);
-    }
+    tmp[0]
+}
+/// Read two `u8`'s from a buffer and create a `u16` from the bytes in BigEndian order.
+///
+/// The first 8 bytes of the u16 are made by the first u8 read, and the second one make the last 8
+/// ```text
+/// u16 => [first_u8][second_u8]
+/// ```
+/// # Argument
+///  - reader: A mutable reference to anything that implements `Read` trait
+///
+/// # Returns
+/// - the u16 value created from the two u8's
+///
+/// # Panics
+/// When the bytes cannot be read
+#[inline]
+pub fn read_u16_be<R>(reader: &mut BufReader<R>) -> Result<u16, DecodeErrors>
+where
+    R: Read,
+{
+    let mut tmp: [u8; 2] = [0, 0];
+    if reader.read(&mut tmp).expect("could not read from data") != 2 {
+        return Err(DecodeErrors::ExhaustedData);
+    };
+    let v = u16::from_be_bytes(tmp);
+    Ok(v)
+}
 
-    decoded
+/// Read `buf.len()*2` data from the underlying `u8` buffer and convert it into u16, and store it into
+/// `buf`
+///
+/// # Arguments
+/// - reader: A mutable reference to the underlying reader.
+/// - buf: A mutable reference to a slice containing u16's
+#[inline]
+pub fn read_u16_into<R>(reader: &mut BufReader<R>, buf: &mut [u16]) -> Result<(), DecodeErrors>
+where
+    R: Read,
+{
+    let mut reader = reader;
+
+    for i in buf {
+        *i = read_u16_be(&mut reader)?
+    }
+    Ok(())
+}
+/// Multiply elements in two vectors
+///
+/// # Arguments
+/// - `a`: A vector containing 64 elements
+/// - `b` :Another vector containing 64 elements
+///
+/// # Returns
+/// Another vector containing multiplied coefficients
+#[inline]
+pub fn dequantize<T>(a: &[i16; 64], b: &[i16; 64]) -> [T; 64]
+where
+    T: Copy + Default + From<i16>,
+{
+    // some decoders put this inside IDCT, i prefer it here
+    let mut c = [T::default(); 64];
+    // using `i32::from` makes the generated assembly first convert before multiplying
+    // which does not favour us at all
+    for i in 0..c.len() {
+        *c.get_mut(i).unwrap() = T::from(a.get(i).unwrap() * b.get(i).unwrap());
+    }
+    c
 }
