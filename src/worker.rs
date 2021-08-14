1a90e5ce8943eaaac5ba23ef047ed75654244ba5
caleb <etemesicaleb@gmail.com>
2021-08-14 13:59:32 +0300

    1. Code refactoring

--- src/worker.rs
+++ src/worker.rs
@@ -1,80 +1,55 @@
-use std::convert::TryInto;
+use crate::Decoder;
 
-use crate::misc::Aligned32;
+impl Decoder {
+    pub(crate) fn color_convert_ycbcr(&mut self, position: usize, output: &mut Vec<u8>) {
+        // The logic here is a bit hard
+        // The reason is how MCUs are designed, in the input every 64 step represents a MCU
+        // but MCU's traverse rows  so we have to do weird skipping and slicing( which is bad cache wise)
+        let mut pos = 0;
 
-/// Carry out dequantization and IDCT for component data
-///
-/// # Arguments
-/// - component :`mut &[i32]`  A mutable reference to a component data. Results after dequantization
-///  and IDCT will be written back in the source allowing us to reuse data
-/// - QT table: A quantization table for this component
-/// - func: A mutable function that can carry out dequantization and IDCT, this allows us to change
-/// the function carrying out IDCT to faster(or slower) implementations depending on the target machine
-#[inline(always)]
-pub fn dequantize_idct_component<F>(component: &mut [i32], qt_table: &[i32; 64], func: &mut F)
-where
-    F: FnMut(&mut [i32; 64], &[i32; 64]),
-{
-    for i in component.chunks_exact_mut(64) {
-        // reuse memory by overwriting the old values with dequantized values
-        func(i.try_into().unwrap(), qt_table);
-    }
-}
-/// Carry out up-sampling and color conversion functions
-///
-/// # Arguments
-///  - y: Y color component data
-///  - cb: Cb color component data
-///  - cr: Cr component data
-///  - convert_function: A function capable of converting from YCBCr to RGB colorspace
-///  - position: The offset from where to write our data
-///  - output: The area to write data on the colorspace
-#[inline(always)]
-pub fn upsample_color_convert_ycbcr<F>(
-    y: &Aligned32<&[i32]>,
-    cb: &Aligned32<&[i32]>,
-    cr: &Aligned32<&[i32]>,
-    convert_function: &mut F,
-    position: usize,
-    output: &mut [u8],
-) where
-    F: FnMut(&[i32], &[i32], &[i32], &mut [u8], usize),
-{
-    // The logic here is a bit hard
-    // The reason is how MCUs are designed, in the input every 64 step represents a MCU
-    // but MCU's traverse rows  so we have to do weird skipping and slicing( which is bad cache wise)
+        let len = self.mcu_block[0].len();
+        // for AVX, we can process 16 items for each color_convert function
+        // but we do even more weird slicing and a lot of other stuff that increases
+        // complexity
 
-    // iterate 8 times
-    let mut pos = 0;
-    // How many MCU's do we have?
-    let mcu_count = y.0.len() / 64;
-    let mut position = position;
-    for i in 0..8 {
-        // depending on the value of i chunk data
-        for _ in 0..mcu_count {
-            #[cfg(feature = "perf")]
-            {
-                // SAFETY: Position can never go out of bounds
-                unsafe {
-                    let y_c = y.0.get_unchecked(pos..pos + 8);
-                    let cb_c = cb.0.get_unchecked(pos..pos + 8);
-                    let cr_c = cr.0.get_unchecked(pos..pos + 8);
-                    convert_function(y_c, cb_c, cr_c, output, position);
-                }
+        // slice into 128(2 mcu's)
+        let mcu_count = len / 128;
+        // check if we have an MCU remaining
+        let remainder = (len % 128) != 0;
+
+        let mut position = position;
+        for i in 0..8 {
+            // Process MCU's in batches of 2, this allows us (where applicable) to convert two MCU rows
+            // using fewer instructions
+            for _ in 0..mcu_count {
+                // Here lies bad ugly slicing, and the cause of 75% of the cache misses...
+
+                // First MCU
+                let y_c = &self.mcu_block[0][pos..pos + 8];
+                let cb_c = &self.mcu_block[1][pos..pos + 8];
+                let cr_c = &self.mcu_block[2][pos..pos + 8];
+
+                //  Second MCU
+                let y1_c = &self.mcu_block[0][pos + 64..pos + 72];
+                let cb2_c = &self.mcu_block[1][pos + 64..pos + 72];
+                let cr2_c = &self.mcu_block[2][pos + 64..pos + 72];
+                // Call color convert function
+                (self.color_convert_16)(y_c, y1_c, cb_c, cb2_c, cr_c, cr2_c, output, position);
+                position += 48;
+                // increase pos by 128, skip 2 MCU's
+
+                pos += 128;
             }
-            #[cfg(not(feature = "perf"))]
-                {
-                    let y_c = &y.0[pos..pos + 8];
-                    let cb_c = &cb.0[pos..pos + 8];
-                    let cr_c = &cr.0[pos..pos + 8];
-                    convert_function(y_c, cb_c, cr_c, output, position);
-                }
-            // increase position by 24, because we wrote 24 RGB values to array
-            position += 24;
-            // increase pos by 64, we skip an mcu
-            pos += 64;
+            pos = (i + 1) * 8;
+            if remainder {
+                // last odd MCU in the column
+                let y_c = &self.mcu_block[0][len - 64..len];
+                let cb_c = &self.mcu_block[1][len - 64..len];
+                let cr_c = &self.mcu_block[2][len - 64..len];
+                // convert function should be able to handle
+                // last mcu
+                (self.color_convert)(y_c, cb_c, cr_c, output, position);
+            }
         }
-        // reset pos to i*8
-        pos = (i + 1) * 8;
     }
 }
