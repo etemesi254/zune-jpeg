685a32e284dece0a4e94668ab8ce9d8ac5e06b40
caleb <etemesicaleb@gmail.com>
2021-08-29 20:21:02 +0300

    Fixed a bug that allowed overwriting of pixels when image width was not a multiple of 8

--- src/worker.rs
+++ src/worker.rs
@@ -1,55 +1,70 @@
 use crate::Decoder;
 
 impl Decoder {
-    pub(crate) fn color_convert_ycbcr(&mut self, position: usize, output: &mut Vec<u8>) {
+    #[allow(clippy::similar_names)]
+    pub(crate) fn color_convert_ycbcr(
+        &mut self,
+        position: &mut usize,
+        output: &mut Vec<u8>,
+        mcu_len: usize,
+    ) {
         // The logic here is a bit hard
         // The reason is how MCUs are designed, in the input every 64 step represents a MCU
         // but MCU's traverse rows  so we have to do weird skipping and slicing( which is bad cache wise)
         let mut pos = 0;
 
-        let len = self.mcu_block[0].len();
-        // for AVX, we can process 16 items for each color_convert function
-        // but we do even more weird slicing and a lot of other stuff that increases
-        // complexity
-
         // slice into 128(2 mcu's)
-        let mcu_count = len / 128;
+        //println!("{}",mcu_len);
+        let mcu_count = (mcu_len - 1) / 2;
         // check if we have an MCU remaining
-        let remainder = (len % 128) != 0;
-
-        let mut position = position;
+        let remainder = ((mcu_len - 1) % 2) != 0;
+        let mcu_width = usize::from(self.info.width) * self.output_colorspace.num_components();
+        let mut expected_pos = *position + mcu_width;
         for i in 0..8 {
             // Process MCU's in batches of 2, this allows us (where applicable) to convert two MCU rows
             // using fewer instructions
+            //println!("{},{}",position,pos);
             for _ in 0..mcu_count {
-                // Here lies bad ugly slicing, and the cause of 75% of the cache misses...
+                //This isn't cache efficient as it hops around too much
 
-                // First MCU
+                // 8 pixels of the first  MCU
                 let y_c = &self.mcu_block[0][pos..pos + 8];
                 let cb_c = &self.mcu_block[1][pos..pos + 8];
                 let cr_c = &self.mcu_block[2][pos..pos + 8];
-
-                //  Second MCU
+                //  8 pixels of the second MCU
                 let y1_c = &self.mcu_block[0][pos + 64..pos + 72];
                 let cb2_c = &self.mcu_block[1][pos + 64..pos + 72];
                 let cr2_c = &self.mcu_block[2][pos + 64..pos + 72];
                 // Call color convert function
                 (self.color_convert_16)(y_c, y1_c, cb_c, cb2_c, cr_c, cr2_c, output, position);
-                position += 48;
                 // increase pos by 128, skip 2 MCU's
-
                 pos += 128;
             }
-            pos = (i + 1) * 8;
+
             if remainder {
                 // last odd MCU in the column
-                let y_c = &self.mcu_block[0][len - 64..len];
-                let cb_c = &self.mcu_block[1][len - 64..len];
-                let cr_c = &self.mcu_block[2][len - 64..len];
+                let y_c = &self.mcu_block[0][pos..pos + 8];
+                let cb_c = &self.mcu_block[1][pos..pos + 8];
+                let cr_c = &self.mcu_block[2][pos..pos + 8];
                 // convert function should be able to handle
                 // last mcu
                 (self.color_convert)(y_c, cb_c, cr_c, output, position);
+                //*position+=24;
             }
+
+            // Sometimes Color convert may overshoot, IE if the image is not
+            // divisible by 8 it may have to pad the last MCU with extra pixels
+            // The decoder is supposed to discard these extra bits
+            //
+            // But instead of discarding those bits, I just tell the color_convert to overwrite them
+            // Meaning I have to reset position to the expected position, which is the width
+            // of the MCU.
+
+            *position = expected_pos;
+            expected_pos += mcu_width;
+
+            // Reset position to start fetching from the next MCU
+            pos = (i + 1) * 8;
         }
     }
 }
