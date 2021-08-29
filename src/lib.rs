a3f73a660b00cdb1c2e15c6ac77f6fe391fc29aa
caleb <etemesicaleb@gmail.com>
2021-08-29 20:17:02 +0300

    Formatting with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-#![allow(clippy::needless_return,clippy::similar_names)]
-#![warn(clippy::correctness, clippy::perf, clippy::pedantic,clippy::inline_always)]
+#![allow(
+    clippy::needless_return,
+    clippy::similar_names,
+    clippy::inline_always,
+    clippy::similar_names
+)]
+#![warn(
+    clippy::correctness,
+    clippy::perf,
+    clippy::pedantic,
+    clippy::inline_always
+)]
 #[macro_use]
 extern crate log;
 
@@ -21,4 +31,14 @@ mod color_convert;
 mod unsafe_utils;
 mod worker;
 
+#[test]
+fn decode_jpeg() {
+    let mut x = Decoder::new();
+    x.set_output_colorspace(ColorSpace::RGBX);
+    let image = x
+        .decode_file("/home/caleb/Pictures/backgrounds/wallpapers/backgrounds/Mr. Lee.jpg")
+        .unwrap();
 
+    println!("{:?}", &image[0..30]);
+    //println!("{:?}", &image[10000..10512]);
+}
