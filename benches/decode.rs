08edf4b05592ea8520fe65e746ab4bce13065301
caleb <etemesicaleb@gmail.com>
2021-09-02 21:19:04 +0300

    Benchmarks

--- benches/decode.rs
+++ benches/decode.rs
@@ -1,36 +1,51 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use jpeg_decoder::Decoder;
 use std::fs::read;
-use std::io::BufReader;
 use std::time::Duration;
-use zune_jpeg::errors::DecodeErrors;
-use zune_jpeg::Image;
-use zune_jpeg::JPEG;
 
-fn decode(buf: &[u8]) -> Result<Image, DecodeErrors> {
-    let jpeg = JPEG::decode_buffer(buf);
-    return jpeg;
-}
+use zune_jpeg::{ColorSpace, Decoder};
+
 fn decode_jpeg(buf: &[u8]) -> Vec<u8> {
-    let mut d = Decoder::new(BufReader::new(buf));
-    let e = d.decode().unwrap();
-    return e;
+    let mut d = Decoder::new();
+    d.set_output_colorspace(ColorSpace::RGB);
+
+    d.decode_buffer(buf).unwrap()
+}
+
+fn decode_jpeg_mozjpeg(buf: &[u8]) -> Vec<[u8; 4]> {
+    let p = std::panic::catch_unwind(|| {
+        let d = mozjpeg::Decompress::with_markers(mozjpeg::ALL_MARKERS)
+            .from_mem(buf)
+            .unwrap();
+
+
+        // rgba() enables conversion
+        let mut image = d.rgba().unwrap();
+
+
+        let pixels: Vec<[u8; 4]> = image.read_scanlines().unwrap();
+        assert!(image.finish_decompress());
+        pixels
+    })
+    .unwrap();
+    p
 }
 fn criterion_benchmark(c: &mut Criterion) {
-    let a = String::from(env!("CARGO_MANIFEST_DIR")) + "/test-images/test-baseline.jpg";
+    let a = String::from("/home/caleb/Pictures/backgrounds/wallpapers/backgrounds/Mr. Lee.jpg");
     let data = read(a).unwrap();
-    c.bench_function("Baseline JPEG Decoding ZUNE_JPEG", |b| {
-        b.iter(|| black_box(decode(data.as_slice())))
-    });
-    c.bench_function("Baseline JPEG Decoding JPEG-DECODER", |b| {
+
+    c.bench_function("Baseline JPEG Decoding zune-jpeg", |b| {
         b.iter(|| black_box(decode_jpeg(data.as_slice())))
+    });
+
+    c.bench_function("Baseline JPEG Decoding  mozjpeg", |b| {
+        b.iter(|| black_box(decode_jpeg_mozjpeg(data.as_slice())))
     });
 }
 
 criterion_group!(name=benches;
       config={
       let c = Criterion::default();
-        c.measurement_time(Duration::from_secs(60))
+        c.measurement_time(Duration::from_secs(5))
       };
     targets=criterion_benchmark);
 criterion_main!(benches);
