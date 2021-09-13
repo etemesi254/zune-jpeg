e969194028281ced9bfdf99c4fc00b5398168d61
caleb <etemesicaleb@gmail.com>
2021-09-13 22:16:40 +0300

    Initial support for upsampling algorithms
    Now baseline horizontal up-sampler is implemented, the rest are just dummies and mine-fields that will cause panics if used

--- src/upsampler.rs
+++ src/upsampler.rs
@@ -1,3 +1,58 @@
 //! Up-sampling routines
+//!
+//! The main upsampling method is a bi-linear interpolation or a "triangle  filter " or libjpeg turbo
+//! `fancy_upsampling` which is a good compromise between speed and visual quality
+//!
+
+pub use sse::upsample_horizontal_sse;
+
+use crate::unsafe_utils::align_zero_alloc;
+
+mod sse;
+
+/// Carry out vertical   upsampling
+pub fn upsample_vertical(input: &Vec<i16>, output_len: usize) -> Vec<i16> {
 
 
+
+    let mut _out = unsafe {
+        align_zero_alloc::<i16, 32>(output_len)
+    };
+    // i'll figure you out..
+
+
+    return Vec::new();
+}
+
+/// Upsample horizontally
+///
+/// The up-sampling algorithm used is libjpeg-turbo `fancy_upsampling` which is a linear
+/// interpolation or triangle filter, see module docs for explanation
+pub fn upsample_horizontal(input: &Vec<i16>, output_len: usize) -> Vec<i16> {
+    // okay horizontal sub-sampling with  bi-linear interpolation
+    let mut out = unsafe {
+        align_zero_alloc::<i16, 32>(output_len)
+    };
+    out[0] = input[0];
+    out[1] = (input[0] * 3 + input[1] + 2) >> 2;
+    let input_len = input.len();
+    // duplicate, number of MCU's are 2
+    for i in 1..input_len - 1 {
+        let sample = 3 * input[i] + 2;
+        out[i * 2 + 0] = (sample + input[i - 1]) >> 2;
+        out[i * 2 + 1] = (sample + input[i + 1]) >> 2;
+    }
+    // write last mcu
+    out[(input_len - 1) * 2] = (input[input_len - 2] * 3 + input[input_len - 1] + 2) >> 2;
+    out[(input_len - 1) * 2 + 1] = input[input_len - 1];
+    return out;
+}
+
+pub fn upsample_horizontal_vertical(input: &Vec<i16>, _output_len: usize) -> Vec<i16> {
+    return Vec::new();
+}
+
+/// Upsample nothing
+pub fn upsample_no_op(_: &Vec<i16>, _: usize) -> Vec<i16> {
+    return Vec::new();
+}
