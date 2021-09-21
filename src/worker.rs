63178708d34d046c567e81f2388029bd28ea6337
caleb <etemesicaleb@gmail.com>
2021-09-21 19:58:36 +0300

    Initial support for interleaved images which is NOT GOING WELL
    Made improvements to matching ycbcr_to_rgb colorspace conversion. Now it won't convert rouge colorspaces.

--- src/worker.rs
+++ src/worker.rs
@@ -3,7 +3,6 @@ use std::sync::{Arc, Mutex};
 use crate::{ColorConvert16Ptr, ColorConvertPtr, ColorSpace, IDCTPtr, MAX_COMPONENTS};
 use crate::components::Components;
 
-
 /// Handle everything else in jpeg processing that doesn't involve bitstream decoding
 ///
 /// This handles routines for images which are not interleaved for interleaved use post_process_interleaved
@@ -20,30 +19,24 @@ use crate::components::Components;
 /// - mcu_len - Number of MCU's per width
 /// - width - Width of the image.
 /// - position: Offset from which to write the pixels
-#[allow(clippy::too_many_arguments,clippy::cast_sign_loss,clippy::cast_possible_truncation,clippy::doc_markdown)]
-pub (crate) fn post_process_non_interleaved(mut unprocessed: [Vec<i16>; MAX_COMPONENTS],
-                    component_data: &[Components],
-                    idct_func: IDCTPtr,
-                    color_convert_16:ColorConvert16Ptr,
-                    color_convert: ColorConvertPtr,
-                    input_colorspace: ColorSpace, output_colorspace: ColorSpace,
-                    output: Arc<Mutex<Vec<u8>>>,
-                    mcu_len: usize,
-                    width: usize,
-                    mut position: usize) {
+#[allow(clippy::too_many_arguments, clippy::cast_sign_loss, clippy::cast_possible_truncation, clippy::doc_markdown)]
+#[rustfmt::skip]
+pub(crate) fn post_process_non_interleaved(mut unprocessed: [Vec<i16>; MAX_COMPONENTS], component_data: &[Components],
+                                           idct_func: IDCTPtr, color_convert_16: ColorConvert16Ptr,
+                                           color_convert: ColorConvertPtr, input_colorspace: ColorSpace,
+                                           output_colorspace: ColorSpace, output: Arc<Mutex<Vec<u8>>>,
+                                           mcu_len: usize, width: usize, mut position: usize) {
     // carry out IDCT
-
     (0..input_colorspace.num_components()).for_each(|x| {
         (idct_func)(unprocessed[x].as_mut_slice(), &component_data[x].quantization_table);
     });
 
     // color convert
     match (input_colorspace, output_colorspace) {
-
         // YCBCR to RGB(A) colorspace conversion.
-        (ColorSpace::YCbCr, _) => {
-            color_convert_ycbcr(&unprocessed, width, output_colorspace, color_convert_16, color_convert, output, &mut position,  mcu_len);
-
+        (ColorSpace::YCbCr, ColorSpace::RGBX|ColorSpace::RGBA|ColorSpace::RGB) => {
+            color_convert_ycbcr_non_interleaved(&unprocessed, width, output_colorspace,
+                                                color_convert_16, color_convert, output, &mut position, mcu_len, );
         }
         (ColorSpace::GRAYSCALE, ColorSpace::GRAYSCALE) => {
             // for grayscale to grayscale we copy first MCU block(which should contain the Y Luminance channel) to the other
@@ -54,6 +47,7 @@ pub (crate) fn post_process_non_interleaved(mut unprocessed: [Vec<i16>; MAX_COMP
         _ => {}
     }
 }
+
 /// Handle everything else in jpeg processing that doesn't involve bitstream decoding
 ///
 /// This handles routines for images which are interleaved for non-interleaved use post_process_non_interleaved
@@ -70,34 +64,33 @@ pub (crate) fn post_process_non_interleaved(mut unprocessed: [Vec<i16>; MAX_COMP
 /// - mcu_len - Number of MCU's per width
 /// - width - Width of the image.
 /// - position: Offset from which to write the pixels
-
-#[allow(clippy::too_many_arguments,clippy::cast_sign_loss,clippy::cast_possible_truncation,clippy::doc_markdown,unused_variables)]
+#[allow(clippy::too_many_arguments, clippy::cast_sign_loss, clippy::cast_possible_truncation, clippy::doc_markdown)]
+#[rustfmt::skip]
 pub(crate) fn post_process_interleaved(mut unprocessed: [Vec<i16>; MAX_COMPONENTS],
-                                       component_data: &[Components],
-                                       idct_func: IDCTPtr,
-                                       color_convert_16:ColorConvert16Ptr,
-                                       color_convert: ColorConvertPtr,
-                                       input_colorspace: ColorSpace,
-                                       output_colorspace: ColorSpace,
-                                       output: Arc<Mutex<Vec<u8>>>,
-                                       mcu_len: usize,
-                                       width: usize,
-                                       mut position: usize){
-
+                                       component_data: &[Components], h_samp: usize, v_samp: usize,
+                                       idct_func: IDCTPtr, color_convert_16: ColorConvert16Ptr,
+                                       color_convert: ColorConvertPtr, input_colorspace: ColorSpace,
+                                       output_colorspace: ColorSpace, output: Arc<Mutex<Vec<u8>>>,
+                                       mcu_len: usize, width: usize, mut position: usize) // so many parameters..
+{
     // carry out dequantization and inverse DCT
-    (0..input_colorspace.num_components()).for_each(|z|{
-        idct_func(&mut unprocessed[z],&component_data[z].quantization_table);
+    (0..input_colorspace.num_components()).for_each(|z| {
+        idct_func(&mut unprocessed[z], &component_data[z].quantization_table);
     });
     // carry out upsampling , the return vector overwrites the original vector
-    for i in 1..input_colorspace.num_components(){
-        unprocessed[i]=(component_data[i].up_sampler)(&unprocessed[i],unprocessed[0].len());
+    for i in 1..input_colorspace.num_components() {
+        unprocessed[i] = (component_data[i].up_sampler)(&unprocessed[i], unprocessed[0].len());
     }
     // color convert
     match (input_colorspace, output_colorspace) {
-        // YCBCR to RGB(A) colorspace conversion.
-        (ColorSpace::YCbCr, _) => {
-            color_convert_ycbcr(&unprocessed, width, output_colorspace, color_convert_16, color_convert, output, &mut position,  mcu_len);
 
+        // YCBCR to RGB(A) colorspace conversion.
+
+        // Match keyword is amazing..
+        (ColorSpace::YCbCr, ColorSpace::RGB|ColorSpace::RGBA|ColorSpace::RGBX) => {
+            color_convert_ycbcr_interleaved(&unprocessed, width, h_samp, v_samp,
+                                            output_colorspace, color_convert_16, color_convert,
+                                            output, &mut position, mcu_len);
         }
         (ColorSpace::GRAYSCALE, ColorSpace::GRAYSCALE) => {
             // for grayscale to grayscale we copy first MCU block(which should contain the Y Luminance channel) to the other
@@ -108,26 +101,22 @@ pub(crate) fn post_process_interleaved(mut unprocessed: [Vec<i16>; MAX_COMPONENT
         _ => {}
     }
 }
-#[allow(clippy::similar_names,clippy::too_many_arguments,clippy::needless_pass_by_value)]
-fn color_convert_ycbcr(mcu_block: &[Vec<i16>; MAX_COMPONENTS],
-                       width: usize,
-                       output_colorspace: ColorSpace,
-                       color_convert_16: ColorConvert16Ptr,
-                       color_convert: ColorConvertPtr,
-                       output: Arc<Mutex<Vec<u8>>>,
-                       position: &mut usize,
-                       mcu_len: usize) {
+
+/// Perform color conversion for non interleaved image
+#[allow(clippy::similar_names, clippy::too_many_arguments, clippy::needless_pass_by_value)]
+#[rustfmt::skip]
+fn color_convert_ycbcr_non_interleaved(mcu_block: &[Vec<i16>; MAX_COMPONENTS], width: usize, output_colorspace: ColorSpace,
+                                       color_convert_16: ColorConvert16Ptr, color_convert: ColorConvertPtr, output: Arc<Mutex<Vec<u8>>>,
+                                       position: &mut usize, mcu_len: usize) {
     // The logic here is a bit hard
     // The reason is how MCUs are designed, in the input every 64 step represents a MCU
     // but MCU's traverse rows  so we have to do weird skipping and slicing( which is bad cache wise)
     let mut pos = 0;
 
     // slice into 128(2 mcu's)
-    //println!("{}",mcu_len);
-    let mcu_count = mcu_len  >> 1;
-    //println!("{}",mcu_count);
+    let mcu_count = (mcu_len - 1) >> 1;
     // check if we have an MCU remaining
-    let remainder = ((mcu_len ) % 2) != 0;
+    let remainder = ((mcu_len - 1) % 2) != 0;
     let mcu_width = width * output_colorspace.num_components();
     let mut expected_pos = *position + mcu_width;
     // Lock here because we do not want to keep locking while writing,
@@ -136,7 +125,6 @@ fn color_convert_ycbcr(mcu_block: &[Vec<i16>; MAX_COMPONENTS],
     for i in 0..8 {
         // Process MCU's in batches of 2, this allows us (where applicable) to convert two MCU rows
         // using fewer instructions
-        //println!("{},{}",position,pos);
         for _ in 0..mcu_count {
             //This isn't cache efficient as it hops around too much
 
@@ -156,7 +144,7 @@ fn color_convert_ycbcr(mcu_block: &[Vec<i16>; MAX_COMPONENTS],
                 let cr2_c = mcu_block[2].get_unchecked(pos + 64..pos + 72);
                 // Call color convert function
                 (color_convert_16)(y_c, y1_c, cb_c, cb2_c, cr_c, cr2_c, &mut **x, position);
-                // increase pos by 128, skip 2 MCU's
+
             }
             pos += 128;
         }
@@ -182,10 +170,95 @@ fn color_convert_ycbcr(mcu_block: &[Vec<i16>; MAX_COMPONENTS],
 
         *position = expected_pos;
         expected_pos += mcu_width;
-      //  println!("{}",position);
+        //  println!("{}",position);
 
         // Reset position to start fetching from the next MCU
-        pos = (i + 1) * 8;
-
+        pos = i*8;
     }
+}
+
+/// Do color-conversion for interleaved MCU
+#[allow(clippy::similar_names, clippy::too_many_arguments, clippy::needless_pass_by_value)]
+#[rustfmt::skip]
+fn color_convert_ycbcr_interleaved(mcu_block: &[Vec<i16>; MAX_COMPONENTS],
+                                   width: usize, h_max: usize, v_max: usize,
+                                   output_colorspace: ColorSpace, color_convert_16: ColorConvert16Ptr,
+                                   color_convert: ColorConvertPtr, output: Arc<Mutex<Vec<u8>>>,
+                                   position: &mut usize, mcu_len: usize) {
+    // The logic here is still a bit hard
+    //let mut pos = 0;
+    // how many (MCU's*2) do we have?
+    let mcu_count = mcu_len >> 1;
+    // check if we have an MCU remaining, i.e there are odd mcu's
+    let remainder = ((mcu_len) % 2) != 0;
+    let mcu_width = width * output_colorspace.num_components();
+    let mut expected_pos = *position + mcu_width;
+    let mut tmp_output = output.lock().unwrap();
+    let mut pos = 0;
+
+    // iterate the number of MCU rows because  up-sampled images can write more than 1 mcu
+    // write an MCU row
+    for _ in 0..h_max * v_max {
+
+        for i in 0..8 {
+
+        // Process MCU's in batches of 2, this allows us (where applicable) to convert two MCU rows
+        // using fewer instructions
+
+        // fetch and parse one mcu line..
+        for _ in 0..mcu_count - 1 {
+
+                //This isn't cache efficient as it hops around too much
+
+                // SAFETY
+                // 1. mcu_block is initialized, (note not assigned) with zeroes
+                // enough to ensure that this is unsafe,
+                // The bounds here can never go above the length
+                unsafe {
+                    // remove some cmp instructions that were slowing us down
+
+                    let y_c = &mcu_block[0][pos..pos + 8];
+                    let cb_c = mcu_block[1].get_unchecked(pos..pos + 8);
+                    let cr_c = mcu_block[2].get_unchecked(pos..pos + 8);
+                    //  8 pixels of the second MCU
+                    let y1_c = &mcu_block[0].get_unchecked(pos + 64..pos + 72);
+                    let cb2_c = mcu_block[1].get_unchecked(pos + 64..pos + 72);
+                    let cr2_c = mcu_block[2].get_unchecked(pos + 64..pos + 72);
+                    for _ in 0..h_max * v_max {
+                        (color_convert_16)(y_c, y1_c, cb_c, cb2_c, cr_c, cr2_c, &mut **tmp_output, position);
+                }
+            }
+            pos += 128;
+            if remainder {
+                // last odd MCU in the column
+                let y_c = &mcu_block[0][pos..pos + 8];
+                let cb_c = &mcu_block[1][pos..pos + 8];
+                let cr_c = &mcu_block[2][pos..pos + 8];
+                // convert function should be able to handle
+                // last mcu
+                (color_convert)(y_c, cb_c, cr_c, &mut **tmp_output, position);
+                //*position+=24;
+            }
+        }
+
+        // Sometimes Color convert may overshoot, IE if the image is not
+        // divisible by 8 it may have to pad the last MCU with extra pixels
+        // The decoder is supposed to discard these extra bits
+        //
+        // But instead of discarding those bits, I just tell the color_convert to overwrite them
+        // Meaning I have to reset position to the expected position, which is the width
+        // of the MCU.
+
+        *position = expected_pos;
+        expected_pos += mcu_width;
+        //  println!("{}",position);
+
+        // Reset position to start fetching from the next MCU line
+        pos = i * 8;
+        //println!("{}",pos);
+        //
+        // next row?
+    }
+    }
+    //panic!();
 }
