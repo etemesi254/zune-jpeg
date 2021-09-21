2bc3c28bf778d93e69922a333bc4623aa638601b
caleb <etemesicaleb@gmail.com>
2021-09-21 19:49:13 +0300

    Added a function to choose the most appropriate color convert function depending on CPU architecture.
    Added table lookups for ycbcr to RGBA/X color conversions

--- src/color_convert.rs
+++ src/color_convert.rs
@@ -5,7 +5,7 @@
     clippy::cast_sign_loss,
     clippy::cast_possible_wrap,
     clippy::too_many_arguments,
-clippy::doc_markdown
+    clippy::doc_markdown
 )]
 
 //! YUV to RGB Conversion
@@ -21,9 +21,12 @@ clippy::doc_markdown
 use std::cmp::{max, min};
 
 #[cfg(feature = "x86")]
-pub use crate::color_convert::avx::{ycbcr_to_rgb_avx2, ycbcr_to_rgba, ycbcr_to_rgbx};
+pub use crate::color_convert::avx::{ycbcr_to_rgb_avx2, ycbcr_to_rgba_avx, ycbcr_to_rgbx_avx2};
 #[cfg(feature = "x86")]
-pub use crate::color_convert::sse::{ycbcr_to_rgb_sse,ycbcr_to_rgb_sse_16,ycbcr_to_rgba_sse_16};
+pub use crate::color_convert::sse::{
+    ycbcr_to_rgb_sse, ycbcr_to_rgb_sse_16, ycbcr_to_rgba_sse, ycbcr_to_rgba_sse_16,
+};
+use crate::{ColorConvert16Ptr, ColorConvertPtr, ColorSpace};
 
 pub mod avx;
 pub mod sse;
@@ -80,10 +83,14 @@ const CB_B: [i32; 256] = ALL_TABLES.3;
 /// - We use a slow version of clamping, that is possible of clamping 1 value at a time
 pub fn ycbcr_to_rgb(y: &[i16], cb: &[i16], cr: &[i16], output: &mut [u8], pos: &mut usize) {
     let pos = pos;
-    for (y, (cb, cr)) in y.iter().zip(cb.iter().zip(cr.iter())) {
-        let r = y + CR_R[*cr as usize] as i16;
-        let g = y - ((CB_G[*cb as usize] + CR_G[*cr as usize]) >> 5) as i16;
-        let b = y + (CB_B[*cb as usize]) as i16;
+    for (y, (cb_i, cr_i)) in y.iter().zip(cb.iter().zip(cr.iter())) {
+
+        let r = y + CR_R[*cr_i as usize] as i16;
+        
+        //print!("{}",*cb_i);
+    
+        let g = y - ((CB_G[(*cb_i as usize)] + CR_G[(*cr_i as usize)&255]) >> 5) as i16;
+        let b = y + (CB_B[(*cb_i as usize)&255]) as i16;
         output[*pos] = clamp(r);
         output[*pos + 1] = clamp(g);
         output[*pos + 2] = clamp(b);
@@ -91,6 +98,35 @@ pub fn ycbcr_to_rgb(y: &[i16], cb: &[i16], cr: &[i16], output: &mut [u8], pos: &
     }
 }
 
+pub fn ycbcr_to_rgba(y: &[i16], cb: &[i16], cr: &[i16], output: &mut [u8], pos: &mut usize) {
+    let pos = pos;
+    for (y, (cb, cr)) in y.iter().zip(cb.iter().zip(cr.iter())) {
+        let r = y + CR_R[*cr as usize] as i16;
+        let g = y - ((CB_G[*cb as usize] + CR_G[*cr as usize]) >> 5) as i16;
+        let b = y + (CB_B[*cb as usize]) as i16;
+        output[*pos] = clamp(r);
+        output[*pos + 1] = clamp(g);
+        output[*pos + 2] = clamp(b);
+        output[*pos + 3] = 255;
+        *pos += 4;
+    }
+}
+/// YcbCr to RGBA color conversion
+pub fn ycbcr_to_rgba_16(
+    y: &[i16],
+    y2: &[i16],
+    cb: &[i16],
+    cb2: &[i16],
+    cr: &[i16],
+    cr2: &[i16],
+    output: &mut [u8],
+    pos: &mut usize,
+) {
+    // first mcu
+    ycbcr_to_rgba(y, cb, cr, output, pos);
+    // second MCU
+    ycbcr_to_rgba(y2, cb2, cr2, output, pos);
+}
 pub fn ycbcr_to_rgb_16(
     y: &[i16],
     y2: &[i16],
@@ -102,23 +138,54 @@ pub fn ycbcr_to_rgb_16(
     pos: &mut usize,
 ) {
     // first mcu
-    for (y, (cb, cr)) in y.iter().zip(cb.iter().zip(cr.iter())) {
-        let r = y + CR_R[*cr as usize] as i16;
-        let g = y - ((CB_G[*cb as usize] + CR_G[*cr as usize]) >> 5) as i16;
-        let b = y + (CB_B[*cb as usize]) as i16;
-        output[*pos] = clamp(r);
-        output[*pos + 1] = clamp(g);
-        output[*pos + 2] = clamp(b);
-        *pos += 3;
+    ycbcr_to_rgb(y, cb, cr, output, pos);
+    // second MCU
+    ycbcr_to_rgb(y2, cb2, cr2, output, pos);
+}
+
+/// This function determines the best color-convert function to carry out
+/// based on the colorspace needed
+pub fn choose_ycbcr_to_rgb_convert_func(
+    type_need: ColorSpace,
+) -> Option<(ColorConvert16Ptr, ColorConvertPtr)> {
+    #[cfg(feature = "x86")]
+    {
+        if is_x86_feature_detected!("avx2") {
+            debug!("Using AVX optimised color conversion functions");
+            // I believe avx2 means sse4 is also available
+            // match colorspace
+            return match type_need {
+                ColorSpace::RGB => {
+                    // Avx for 16, sse for 8
+                    Some((ycbcr_to_rgb_avx2, ycbcr_to_rgb_sse))
+                }
+                ColorSpace::RGBA => {
+                    // Avx for 16, sse for 8
+                    Some((ycbcr_to_rgba_avx, ycbcr_to_rgba_sse))
+                }
+                ColorSpace::RGBX => Some((ycbcr_to_rgbx_avx2, ycbcr_to_rgba_sse)),
+                _ => None,
+            };
+        }
+        // try sse
+        else if is_x86_feature_detected!("sse4.1") {
+            // I believe avx2 means sse4 is also available
+            // match colorspace
+            debug!("No support for avx2 switching to sse");
+            debug!("Using sse color convert functions");
+            return match type_need {
+                ColorSpace::RGB => Some((ycbcr_to_rgb_sse_16, ycbcr_to_rgb_sse)),
+                ColorSpace::RGBA | ColorSpace::RGBX => {
+                    Some((ycbcr_to_rgba_sse_16, ycbcr_to_rgba_sse))
+                }
+                _ => None,
+            };
+        }
     }
-    // Second MCU
-    for (y, (cb, cr)) in y2.iter().zip(cb2.iter().zip(cr2.iter())) {
-        let r = y + CR_R[*cr as usize] as i16;
-        let g = y - ((CB_G[*cb as usize] + CR_G[*cr as usize]) >> 5) as i16;
-        let b = y + (CB_B[*cb as usize]) as i16;
-        output[*pos] = clamp(r);
-        output[*pos + 1] = clamp(g);
-        output[*pos + 2] = clamp(b);
-        *pos += 3;
-    }
+    // when there is no x86 or we haven't returned by here, resort to lookup tables
+    return match type_need {
+        ColorSpace::RGB => Some((ycbcr_to_rgb_16, ycbcr_to_rgb)),
+        ColorSpace::RGBA | ColorSpace::RGBX => Some((ycbcr_to_rgba_16, ycbcr_to_rgba)),
+        _ => None,
+    };
 }
