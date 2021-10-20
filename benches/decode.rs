7196f74ef5b848714b69e53ce4d0727140c972b2
caleb <etemesicaleb@gmail.com>
2021-10-20 11:48:27 +0300

    Removed horizontal sub-sampling benchmarks

--- benches/decode.rs
+++ benches/decode.rs
@@ -1,14 +1,13 @@
 //! Benchmarks for
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs::read;
 use std::time::Duration;
 
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use zune_jpeg::{ColorSpace, Decoder};
 
 fn decode_jpeg(buf: &[u8]) -> Vec<u8>
 {
-
     let mut d = Decoder::new();
 
     d.set_output_colorspace(ColorSpace::RGB);
@@ -18,9 +17,7 @@ fn decode_jpeg(buf: &[u8]) -> Vec<u8>
 
 fn decode_jpeg_mozjpeg(buf: &[u8]) -> Vec<[u8; 3]>
 {
-
     let p = std::panic::catch_unwind(|| {
-
         let d = mozjpeg::Decompress::with_markers(mozjpeg::ALL_MARKERS)
             .from_mem(buf)
             .unwrap();
@@ -41,7 +38,6 @@ fn decode_jpeg_mozjpeg(buf: &[u8]) -> Vec<[u8; 3]>
 
 fn decode_jpeg_image_rs(buf: &[u8]) -> Vec<u8>
 {
-
     let mut decoder = jpeg_decoder::Decoder::new(buf);
 
     decoder.decode().unwrap()
@@ -49,46 +45,40 @@ fn decode_jpeg_image_rs(buf: &[u8]) -> Vec<u8>
 
 fn criterion_benchmark(c: &mut Criterion)
 {
-
     let a = env!("CARGO_MANIFEST_DIR").to_string() + "/test-images/speed_bench.jpg";
 
     let data = read(a).unwrap();
 
     c.bench_function("Baseline JPEG Decoding zune-jpeg", |b| {
-
         b.iter(|| black_box(decode_jpeg(data.as_slice())))
     });
 
     c.bench_function("Baseline JPEG Decoding  mozjpeg", |b| {
-
         b.iter(|| black_box(decode_jpeg_mozjpeg(data.as_slice())))
     });
 
     c.bench_function("Baseline JPEG Decoding  imagers/jpeg-decoder", |b| {
-
         b.iter(|| black_box(decode_jpeg_image_rs(data.as_slice())))
     });
 
-    let x = read(
-        env!("CARGO_MANIFEST_DIR").to_string()
-            + "/test-images/speed_bench_horizontal_subsampling.jpg",
-    )
-    .unwrap();
-
-    c.bench_function("Horizontal sampling JPEG Decoding zune-jpeg", |b| {
-
-        b.iter(|| black_box(decode_jpeg(x.as_slice())))
-    });
-
-    c.bench_function("Horizontal sampling JPEG Decoding  mozjpeg", |b| {
-
-        b.iter(|| black_box(decode_jpeg_mozjpeg(x.as_slice())))
-    });
-
-    c.bench_function(
-        "Horizontal sampling JPEG Decoding  imagers/jpeg-decoder",
-        |b| b.iter(|| black_box(decode_jpeg_image_rs(x.as_slice()))),
-    );
+    // let x = read(
+    //     env!("CARGO_MANIFEST_DIR").to_string()
+    //         + "/test-images/speed_bench_horizontal_subsampling.jpg",
+    // )
+    // .unwrap();
+    //
+    // c.bench_function("Horizontal sampling JPEG Decoding zune-jpeg", |b| {
+    //     b.iter(|| black_box(decode_jpeg(x.as_slice())))
+    // });
+    //
+    // c.bench_function("Horizontal sampling JPEG Decoding  mozjpeg", |b| {
+    //     b.iter(|| black_box(decode_jpeg_mozjpeg(x.as_slice())))
+    // });
+    //
+    // c.bench_function(
+    //     "Horizontal sampling JPEG Decoding  imagers/jpeg-decoder",
+    //     |b| b.iter(|| black_box(decode_jpeg_image_rs(x.as_slice()))),
+    // );
 }
 
 criterion_group!(name=benches;
