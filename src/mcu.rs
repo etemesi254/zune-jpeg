db9edffe91fd394440fa37d3e70c5cf452f3b2a5
caleb <etemesicaleb@gmail.com>
2021-10-20 11:22:23 +0300

    Refactored DC and AC table checks to a different function.

--- src/mcu.rs
+++ src/mcu.rs
@@ -16,74 +16,8 @@ pub const DCT_BLOCK: usize = 64;
 
 impl Decoder
 {
-    /// Decode an MCU with no interleaving aka the components were not down-sampled
-    /// hence are arranged in Y,Cb,Cr fashion
-    #[allow(clippy::similar_names)]
-    #[inline(never)]
-    #[rustfmt::skip]
-    pub(crate) fn decode_mcu_ycbcr_baseline(&mut self,
-                                            reader: &mut Cursor<Vec<u8>>,
-    ) -> Result<Vec<u8>, DecodeErrors> {
-        #[cfg(feature = "threadpool")]
-            let pool = threadpool::ThreadPool::default();
-
-        let (mcu_width, mcu_height);
-        if self.interleaved {
-            // set upsampling functions
-            self.set_upsampling()?;
-
-            mcu_width = self.mcu_x;
-
-            mcu_height = self.mcu_y;
-        } else {
-            mcu_width = ((self.info.width + 7) / 8) as usize;
-
-            mcu_height = ((self.info.height + 7) / 8) as usize;
-        }
-
-
-        let mut stream = BitStream::new();
-        // Size of our output image(width*height)
-        let capacity = usize::from(self.info.width + 7) * usize::from(self.info.height + 7);
-
-        let component_capacity = usize::from((self.info.width + 7) / 8) * DCT_BLOCK;
-        // for those pointers storing unprocessed items, zero them out here
-        for (pos, comp) in self.components.iter().enumerate() {
-            // multiply capacity with sampling factor, it  should be 1*1 for un-sampled images
-
-            //NOTE: We only allocate a block if we need it, so e.g for grayscale
-            // we don't allocate for CB and Cr channels
-            if min(self.output_colorspace.num_components() - 1, pos) == pos
-            {
-                self.mcu_block[pos] = vec![0; component_capacity * comp.vertical_sample * comp.horizontal_sample];
-            }
-        }
-        let mut position = 0;
-
-        // Create an Arc of components to prevent cloning on every MCU width
-        let global_component = Arc::new(self.components.clone());
-
-        // Storage for decoded pixels
-        let global_channel = Arc::new(Mutex::new(
-            vec![0; capacity * self.output_colorspace.num_components()]));
-
-        // things needed for post processing that we can remove out of the loop
-        let input = self.input_colorspace;
-
-        let output = self.output_colorspace;
-
-        let idct_func = self.idct_func;
-
-        let color_convert = self.color_convert;
-
-        let color_convert_16 = self.color_convert_16;
-
-        let width = usize::from(self.width());
-
-        let h_max = self.h_max;
-
-        let v_max = self.v_max;
-
+    /// Check for existence of DC and AC Huffman Tables
+    fn check_tables(&self) -> Result<(), DecodeErrors> {
         // check that dc and AC tables exist outside the hot path
         for i in 0..self.input_colorspace.num_components()
         {
@@ -113,66 +47,163 @@ impl Decoder
                     ))
                 })?;
         }
+        Ok(())
+    }
+
+    /// Decode an MCU with no interleaving aka the components were not down-sampled
+    /// hence are arranged in Y,Cb,Cr fashion
+    #[allow(clippy::similar_names)]
+    #[inline(never)]
+    #[rustfmt::skip]
+    pub(crate) fn decode_mcu_ycbcr_baseline(
+        &mut self,
+        reader: &mut Cursor<Vec<u8>>,
+    ) -> Result<Vec<u8>, DecodeErrors>
+    {
+        #[cfg(feature = "threadpool")]
+        let pool = threadpool::ThreadPool::default();
+
+        let (mcu_width, mcu_height);
+
+        if self.interleaved
+        {
+            // set upsampling functions
+            self.set_upsampling()?;
+
+            if self.h_max > self.v_max
+            {
+                // horizontal sub-sampling.
+
+                // Values for horizontal samples end halfway the image and do not complete an MCU width.
+                // To make it complete we multiply width by 2( 1/2 * 2 -> 1) and divide mcu_y by 2
+                mcu_width = self.mcu_x * 2;
+
+                mcu_height = self.mcu_y / 2;
+            } else {
+                mcu_width = self.mcu_x;
+
+                mcu_height = self.mcu_y;
+            }
+        } else {
+            mcu_width = ((self.info.width + 7) / 8) as usize;
+
+            mcu_height = ((self.info.height + 7) / 8) as usize;
+        }
+
+        let mut stream = BitStream::new();
+        // Size of our output image(width*height)
+        let capacity = usize::from(self.info.width + 7) * usize::from(self.info.height + 7);
+
+        let component_capacity = mcu_width * DCT_BLOCK;
+        // for those pointers storing unprocessed items, zero them out here
+        for (pos, comp) in self.components.iter().enumerate()
+        {
+            // multiply capacity with sampling factor, it  should be 1*1 for un-sampled images
+
+            //NOTE: We only allocate a block if we need it, so e.g for grayscale
+            // we don't allocate for CB and Cr channels
+            if min(self.output_colorspace.num_components() - 1, pos) == pos
+            {
+                self.mcu_block[pos] =
+                    vec![0; component_capacity * comp.vertical_sample * comp.horizontal_sample];
+            }
+        }
+        let mut position = 0;
+
+        // Create an Arc of components to prevent cloning on every MCU width
+        let global_component = Arc::new(self.components.clone());
+
+        // Storage for decoded pixels
+        let global_channel = Arc::new(Mutex::new(
+            vec![0; capacity * self.output_colorspace.num_components()]));
+
+        // things needed for post processing that we can remove out of the loop
+        let input = self.input_colorspace;
+
+        let output = self.output_colorspace;
+
+        let idct_func = self.idct_func;
+
+        let color_convert = self.color_convert;
+
+        let color_convert_16 = self.color_convert_16;
+
+        let width = usize::from(self.width());
+
+        let h_max = self.h_max;
+
+        let v_max = self.v_max;
+
+        // check dc and AC tables
+        self.check_tables()?;
+
         for _ in 0..mcu_height
         {
+            // Ideally this should be one loop but I'm parallelizing per MCU width boys
             'label: for _ in 0..mcu_width
             {
                 // iterate over components
                 for pos in 0..self.input_colorspace.num_components()
                 {
-                    // if image is interleaved iterate over scan-n components,
-                    // otherwise if it-s non-interleaved, these routines iterate in
-                    // trivial scanline order(Y,Cb,Cr)
                     let component = &mut self.components[pos];
-                    let dc_table = unsafe
-                        {
-                            self.dc_huffman_tables
-                                .get_unchecked(component.dc_huff_table).as_ref()
-                                .unwrap_or_else(|| std::hint::unreachable_unchecked())
-                        };
-                    let ac_table = unsafe {
-                        self.ac_huffman_tables
-                            .get_unchecked(component.ac_huff_table).as_ref()
+                    // Safety:The tables were confirmed to exist in self.check_tables();
+                    let dc_table = unsafe {
+                        self.dc_huffman_tables.get_unchecked(component.dc_huff_table)
+                            .as_ref()
                             .unwrap_or_else(|| std::hint::unreachable_unchecked())
                     };
-                    for _ in 0..component.horizontal_sample{
-                        for _ in 0..component.vertical_sample {
+                    let ac_table = unsafe {
+                        self.ac_huffman_tables.get_unchecked(component.ac_huff_table)
+                            .as_ref()
+                            .unwrap_or_else(|| std::hint::unreachable_unchecked())
+                    };
+                    // If image is interleaved iterate over scan-n components,
+                    // otherwise if it-s non-interleaved, these routines iterate in
+                    // trivial scanline order(Y,Cb,Cr)
+                    for _ in 0..component.vertical_sample * component.horizontal_sample
+                    {
+                        let mut tmp = [0; DCT_BLOCK];
+                        // decode the MCU
+                        if !(stream.decode_mcu_block(reader, dc_table, ac_table, &mut tmp, &mut component.dc_pred))
+                        {
+                            // Found a marker
 
-                            let mut tmp = [0; DCT_BLOCK];
-                            // decode the MCU
-                            if !(stream.decode_mcu_block(reader, dc_table, ac_table, &mut tmp, &mut component.dc_pred))
+                            // Read stream and see what marker is stored there
+                            let marker = stream.marker.expect("No marker found");
+
+                            match marker
                             {
-                                // Found a marker
-
-                                // Read stream and see what marker is stored there
-                                let marker = stream.marker.expect("No marker found");
-
-                                match marker {
-                                    Marker::RST(_) => {
+                                Marker::RST(_) =>
+                                    {
                                         // reset stream
                                         stream.reset();
                                         // Initialize dc predictions to zero for all components
                                         // self.components.iter_mut().for_each(|x| x.dc_pred = 0);
                                         // continue;
                                     }
-                                    Marker::EOI => {
+                                Marker::EOI =>
+                                    {
                                         debug!("EOI marker found, wrapping up here ");
                                         // Okay encountered end of Image break to IDCT and color convert.
                                         break 'label;
                                     }
-                                    _ => {
-                                        return Err(DecodeErrors::MCUError(format!("Marker {:?} found in bitstream, cannot continue", marker)));
+                                _ =>
+                                    {
+                                        return Err(DecodeErrors::MCUError(format!(
+                                            "Marker {:?} found in bitstream, cannot continue",
+                                            marker
+                                        )));
                                     }
-                                }
                             }
-                            // Store only needed components (i.e for YCbCr->Grayscale don't store Cb and Cr channels)
-                            // improves speed when we do a clone(less items to clone)
-                            if min(self.output_colorspace.num_components() - 1, pos) == pos {
-                                let counter = component.counter;
-                                self.mcu_block[pos][counter..counter + 64].copy_from_slice(&tmp);
+                        }
 
-                                component.counter += 64;
-                            }
+                        // Store only needed components (i.e for YCbCr->Grayscale don't store Cb and Cr channels)
+                        // improves speed when we do a clone(less items to clone)
+                        if min(self.output_colorspace.num_components() - 1, pos) == pos
+                        {
+                            let counter = component.counter;
+                            self.mcu_block[pos][counter..counter + 64].copy_from_slice(&tmp);
+                            component.counter += 64;
                         }
                     }
                 }
@@ -188,10 +219,8 @@ impl Decoder
 
             // FIXME: Fix this for single-threaded functions, should not be cloning
             let mut block = self.mcu_block.clone();
-
             #[cfg(feature = "threadpool")]
                 {
-
                     pool.execute(move || {
                         post_process(&mut block, &component, h_max, v_max,
                                      idct_func, color_convert_16, color_convert,
@@ -208,11 +237,14 @@ impl Decoder
                 }
 
             // update position here
-            position += width * 8 * self.output_colorspace.num_components() * h_max * v_max;
+            position += width * output.num_components() * 8 * h_max * v_max;
         }
         // Block this thread until worker threads have finished
         #[cfg(feature = "threadpool")]
-            pool.join();
+            {
+                pool.join();
+            }
+
         debug!("Finished decoding image");
 
         // Global channel may be over allocated for uneven images, shrink it back
