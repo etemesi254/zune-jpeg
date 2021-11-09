165db278dcb20b16a9f4b29a675c9a69ab5bf4b5
caleb <etemesicaleb@gmail.com>
2021-11-09 17:02:39 +0300

    Fixed offset calculation for per width mcu calculation, all coefficients of sub-sampled images should now be written at the correct position

--- src/mcu.rs
+++ src/mcu.rs
@@ -2,7 +2,7 @@
 //!
 //! # Side notes
 //! Yes, I pull in some dubious tricks, like really dubious here, they're not hard to come up
-//!  but I know they're hard to understand(e.g how I don't allocate space for Cb and Cr
+//! but I know they're hard to understand(e.g how I don't allocate space for Cb and Cr
 //! channels if output colorspace is grayscale) but bear with me, it's the search for fast software
 //! that got me here.
 //!
@@ -50,6 +50,7 @@ use std::io::Cursor;
 use std::sync::Arc;
 
 use crate::bitstream::BitStream;
+use crate::components::ComponentID;
 use crate::Decoder;
 use crate::errors::DecodeErrors;
 use crate::marker::Marker;
@@ -106,8 +107,12 @@ impl Decoder
         Ok(())
     }
 
-    /// Decode an MCU with no interleaving aka the components were not down-sampled
-    /// hence are arranged in Y,Cb,Cr fashion
+    /// Decode MCUs and carry out post processing.
+    ///
+    /// This is the main decoder loop for the library aka the hot path.
+    ///
+    /// Because of this, we pull in some very crazy optimization tricks hence readability is a pinch
+    /// here.
     #[allow(clippy::similar_names)]
     #[inline(never)]
     #[rustfmt::skip]
@@ -117,8 +122,10 @@ impl Decoder
     ) -> Result<Vec<u8>, DecodeErrors>
     {
         let mut scoped_pools = scoped_threadpool::Pool::new(num_cpus::get() as u32);
+        info!("Created {} worker threads",scoped_pools.thread_count());
 
         let (mcu_width, mcu_height);
+        let mut bias = 1;
 
         if self.interleaved
         {
@@ -130,20 +137,28 @@ impl Decoder
                 // horizontal sub-sampling.
 
                 // Values for horizontal samples end halfway the image and do not complete an MCU width.
-                // To make it complete we multiply width by 2( 1/2 * 2 -> 1) and divide mcu_y by 2
+                // To make it complete we multiply width by 2 and divide mcu_height by 2
                 mcu_width = self.mcu_x * 2;
 
                 mcu_height = self.mcu_y / 2;
+            } else if self.h_max == self.v_max && self.h_max == 2 {
+                mcu_width = self.mcu_x;
+
+                mcu_height = self.mcu_y / 2;
+                bias = 2;
             } else {
                 mcu_width = self.mcu_x;
 
                 mcu_height = self.mcu_y;
             }
         } else {
+            // For non-interleaved images( (1*1) subsampling)
+            // number of MCU's are the widths (+7 to account for paddings) divided bu 8.
             mcu_width = ((self.info.width + 7) / 8) as usize;
 
             mcu_height = ((self.info.height + 7) / 8) as usize;
         }
+
 
         let mut stream = BitStream::new();
         // Size of our output image(width*height)
@@ -159,8 +174,13 @@ impl Decoder
             // we don't allocate for CB and Cr channels
             if min(self.output_colorspace.num_components() - 1, pos) == pos
             {
+                let mut len = component_capacity * comp.vertical_sample * comp.horizontal_sample;
+                // For 4:2:0 upsampling we need to do some tweaks, reason explained in bias
+                if bias == 2 && comp.component_id == ComponentID::Y {
+                    len *= 2;
+                }
                 self.mcu_block[pos] =
-                    vec![0; component_capacity * comp.vertical_sample * comp.horizontal_sample];
+                    vec![0; len];
             }
         }
 
@@ -188,123 +208,155 @@ impl Decoder
 
         let v_max = self.v_max;
 
-        let stride = self.mcu_block[0].len() >> 1;
+        let stride = (self.mcu_block[0].len()) >> 2;
 
         // check dc and AC tables
         self.check_tables()?;
 
+        // Split output into different blocks each containing enough space for an MCU width
         let mut chunks = global_channel.chunks_exact_mut(width * output.num_components() * 8 * h_max * v_max);
+
         // Argument for scoped threadpools, see file docs.
         scoped_pools.scoped::<_, Result<(), DecodeErrors>>(|scope| {
-        for _ in 0..mcu_height
-        {
-            // Ideally this should be one loop but I'm parallelizing per MCU width boys
-            'label: for j in 0..mcu_width
+            for _ in 0..mcu_height
             {
-                // iterate over components
+                // Bias only affects 4:2:0(chroma quartered) sub-sampled images. So let me explain
+                // For  4:2:0 sub-sampling, we decode 4 rows of MCU's, the hard part is
+                // determining where the Y channel is stored. Dor Y Channel it looks like this
+                // |Y1| |Y2| |Cb| |Cr| |Y5| | Y6|
+                // |Y3| |Y4|           |Y7|  |Y8|
+                // ------------------------------
+                // |Y9|  |Y11|        |Y13| |Y14|
+                // |Y10| |Y12|        |Y15| |Y16|
+                // The problem becomes knowing where to write the channel once decoded.
+                // For vertical / horizontal sub-sampling, we use j(iterator in MCU width).
+                // But j cannot be used to determine 4:2:0 sub-sampled because it will write in the wrong place
+                //
+                // What we want while decoding the first row, write in the first half of the vector(Y component only).
+                // The second row, write in the second half of the vector.
+                // Ideally this means that  our offset calculation must take this into account while remaining transparent
+                // to Cb and Cr channels(those are written differently), and current whether we are in the first
+                // row or second row. And that becomes quite complex, forgive me...
+                for v in 0..bias {
+                    // Ideally this should be one loop but I'm parallelizing per MCU width boys
+                    'label: for j in 0..mcu_width
+                    {
+                        // iterate over components
 
-                'rst: for pos in 0..self.input_colorspace.num_components()
-                {
-                    let component = &mut self.components[pos];
-                    // Safety:The tables were confirmed to exist in self.check_tables();
-                    let dc_table = unsafe {
-                        self.dc_huffman_tables.get_unchecked(component.dc_huff_table)
-                            .as_ref()
-                            .unwrap_or_else(|| std::hint::unreachable_unchecked())
-                    };
-                    let ac_table = unsafe {
-                        self.ac_huffman_tables.get_unchecked(component.ac_huff_table)
-                            .as_ref()
-                            .unwrap_or_else(|| std::hint::unreachable_unchecked())
-                    };
-                    // If image is interleaved iterate over scan  components,
-                    // otherwise if it-s non-interleaved, these routines iterate in
-                    // trivial scanline order(Y,Cb,Cr)
-                    for v_samp in 0..component.vertical_sample {
-                        for h_samp in 0..component.horizontal_sample {
-                            let mut tmp = [0; DCT_BLOCK];
-                            // decode the MCU
-                            if !(stream.decode_mcu_block(reader, dc_table, ac_table, &mut tmp, &mut component.dc_pred))
-                            {
-                                // Found a marker
+                        'rst: for pos in 0..self.input_colorspace.num_components()
+                        {
+                            let component = &mut self.components[pos];
+                            // Safety:The tables were confirmed to exist in self.check_tables();
+                            // Reason.
+                            // - These were 4 branch checks per component, for a 1080 * 1080 *3 component image
+                            //   that becomes(1080*1080*3)/(16)-> 218700 branches in the hot path. And I'm not
+                            //   paying that penalty
+                            let dc_table = unsafe {
+                                self.dc_huffman_tables.get_unchecked(component.dc_huff_table)
+                                    .as_ref()
+                                    .unwrap_or_else(|| std::hint::unreachable_unchecked())
+                            };
+                            let ac_table = unsafe {
+                                self.ac_huffman_tables.get_unchecked(component.ac_huff_table)
+                                    .as_ref()
+                                    .unwrap_or_else(|| std::hint::unreachable_unchecked())
+                            };
+                            // If image is interleaved iterate over scan  components,
+                            // otherwise if it-s non-interleaved, these routines iterate in
+                            // trivial scanline order(Y,Cb,Cr)
+                            for v_samp in 0..component.vertical_sample {
+                                for h_samp in 0..component.horizontal_sample {
+                                    let mut tmp = [0; DCT_BLOCK];
+                                    // decode the MCU
+                                    if !(stream.decode_mcu_block(reader, dc_table, ac_table, &mut tmp, &mut component.dc_pred))
+                                    {
+                                        // Found a marker
+                                        // Read stream and see what marker is stored there
+                                        let marker = stream.marker.expect("No marker found");
 
-                                // Read stream and see what marker is stored there
-                                let marker = stream.marker.expect("No marker found");
+                                        match marker
+                                        {
+                                            Marker::RST(_) =>
+                                                {
+                                                    // reset stream
+                                                    stream.reset();
+                                                    // Initialize dc predictions to zero for all components
+                                                    self.components.iter_mut().for_each(|x| x.dc_pred = 0);
+                                                    // Start iterating again. from position.
+                                                    break 'rst;
+                                                }
+                                            Marker::EOI =>
+                                                {
+                                                    info!("EOI marker found, wrapping up here ");
+                                                    // Okay encountered end of Image break to IDCT and color convert.
+                                                    break 'label;
+                                                }
+                                            _ =>
+                                                {
+                                                    return Err(DecodeErrors::MCUError(format!(
+                                                        "Marker {:?} found in bitstream, possibly corrupt jpeg",
+                                                        marker
+                                                    )));
+                                                }
+                                        }
+                                    }
+                                    // Store only needed components (i.e for YCbCr->Grayscale don't store Cb and Cr channels)
+                                    // improves speed when we do a clone(less items to clone)
+                                    if min(self.output_colorspace.num_components() - 1, pos) == pos
+                                    {
+                                        // calculate where to start writing. This is quite complex because MCU's
+                                        // in images are weird.
+                                        // A good example is vertical sub-sampling(what sent me to this rabbit hole)
+                                        // The run-length encoding is
+                                        // |Y1| |Cb| Cr| |Y3| |Cb| |Cr| |Y5|
+                                        // |Y2|          |Y4|           |Y6|
+                                        //
+                                        // During  decoding, we have to write |Y2| in the right place and this calculation
+                                        // helps us do that.
+                                        // We basically jump halfway our vector for writing |Y2| and jump to the start and an offset for
+                                        // writing |Y3| and rinse and repeat.
 
-                                match marker
-                                {
-                                    Marker::RST(_) =>
-                                        {
-                                            // reset stream
-                                            stream.reset();
-                                            // Initialize dc predictions to zero for all components
-                                            self.components.iter_mut().for_each(|x| x.dc_pred = 0);
-                                            // Start iterating again. from position.
-                                            break 'rst;
-                                        }
-                                    Marker::EOI =>
-                                        {
-                                            debug!("EOI marker found, wrapping up here ");
-                                            // Okay encountered end of Image break to IDCT and color convert.
-                                            break 'label;
-                                        }
-                                    _ =>
-                                        {
-                                            return Err(DecodeErrors::MCUError(format!(
-                                                "Marker {:?} found in bitstream, cannot continue",
-                                                marker
-                                            )));
-                                        }
+                                        // This is the most complex offset calculation in existence
+                                        let is_y = usize::from(component.component_id == ComponentID::Y);
+
+                                        // This only affects 4:2:0 images.
+                                        let y_offset = v * ((stride * is_y)
+                                            + (stride * (component.vertical_sample - 1) * is_y));
+
+                                        // offset calculator.
+                                        let start = (j * 64 * component.horizontal_sample)
+                                            + (h_samp * 64)
+                                            + (stride * v_samp)
+                                            + y_offset;
+                                        self.mcu_block[pos][start..start + 64].copy_from_slice(&tmp);
+                                    }
                                 }
-                            }
-                            // Store only needed components (i.e for YCbCr->Grayscale don't store Cb and Cr channels)
-                            // improves speed when we do a clone(less items to clone)
-                            if min(self.output_colorspace.num_components() - 1, pos) == pos
-                            {
-                                // calculate where to start writing. This is quite complex because MCU's
-                                // in images are weird.
-                                // A good example is vertical sub-sampling(what sent me to this rabbit hole)
-                                // The run-length encoding is
-                                // |Y1| |Cb| Cr| |Y3|
-                                // |Y2|          |Y4|
-                                //
-                                // During  decoding, we have to write |Y2| in the right place and this calculation
-                                // helps us do that.
-                                // We basically jump halfway our vector for writing |Y2| and jump to the start and an offset for
-                                // writing |Y3| and rinse and repeat.
-                                let start = (j*64*component.horizontal_sample) + (h_samp*64)+ (stride * v_samp);
-
-                                self.mcu_block[pos][start..start + 64].copy_from_slice(&tmp);
-
-
                             }
                         }
                     }
                 }
+
+                // Clone things, to make multithreading safe
+                let component = global_component.clone();
+
+
+                let mut block = self.mcu_block.clone();
+
+                let next_chunk = chunks.next().unwrap();
+
+
+                scope.execute(move || {
+                    post_process(&mut block, &component, h_max, v_max,
+                                 idct_func, color_convert_16, color_convert,
+                                 input, output, next_chunk,
+                                 mcu_width, width);
+                });
             }
-
-            // Clone things, to make multithreading safe
-            let component = global_component.clone();
-
-
-            // FIXME: Fix this for single-threaded functions, should not be cloning
-            let mut block = self.mcu_block.clone();
-            let next_chunk = chunks.next().unwrap();
-
-
-            scope.execute(move || {
-            post_process(&mut block, &component, h_max, v_max,
-                         idct_func, color_convert_16, color_convert,
-                         input, output, next_chunk,
-                         mcu_width, width);
-            });
-        }
-        //everything is okay
-         Ok(())
-          })?;
-
-        debug!("Finished decoding image");
-
+            //everything is okay
+            Ok(())
+        })?;
+        info!("Finished decoding image");
+        // remove excess allocation for images.
         global_channel.truncate(
             usize::from(self.width())
                 * usize::from(self.height())
