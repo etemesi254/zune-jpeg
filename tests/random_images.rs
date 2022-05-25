a3aa9689d71b85033f47759be8bff94873b36fb7
caleb <etemesicaleb@gmail.com>
2022-05-25 15:44:39 +0300

    tests: Add more images in tests.

---
+++ tests/random_images.rs
@@ -0,0 +1,56 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+use mozjpeg::ColorSpace as OutColorSpace;
+use zune_jpeg::{ColorSpace, Decoder};
+
+fn write_output(name: &str, pixels: &[u8], width: usize, height: usize, colorspace: OutColorSpace)
+{
+    let output: String = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/outputs/random/";
+    std::fs::create_dir_all(&output).unwrap();
+
+    std::panic::catch_unwind(|| {
+        let mut comp = mozjpeg::Compress::new(colorspace);
+
+        comp.set_size(width, height);
+        comp.set_mem_dest();
+        comp.start_compress();
+
+        assert!(comp.write_scanlines(pixels));
+
+        comp.finish_compress();
+
+        let jpeg_bytes = comp.data_to_vec().unwrap();
+
+        let mut v = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(output.clone() + "/" + name)
+            .unwrap();
+
+        v.write_all(&jpeg_bytes).unwrap();
+
+        // write to file, etc.
+    })
+        .unwrap();
+}
+
+#[test]
+fn huffman_third_index()
+{
+    //
+    let path =
+        env!("CARGO_MANIFEST_DIR").to_string() + "/tests/inputs/huffman_third_index.jpg";
+    let mut decoder = Decoder::new();
+    // Grayscale
+
+    decoder.set_output_colorspace(ColorSpace::GRAYSCALE);
+    let pixels = decoder.decode_file(&path).expect("Test failed decoding");
+    write_output(
+        "huffman_third_index.jpg",
+        &pixels,
+        decoder.width() as usize,
+        decoder.height() as usize,
+        OutColorSpace::JCS_GRAYSCALE,
+    );
+}
