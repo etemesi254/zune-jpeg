050a2327c9eee9cc435a91f6588d67cfc661195b
caleb <etemesicaleb@gmail.com>
2022-06-11 23:22:24 +0300

    fuzz: Run decoding on one thread.
    This improves error messages that arise during multithreaded decoding.

--- fuzz/fuzz_targets/decode_buffer.rs
+++ fuzz/fuzz_targets/decode_buffer.rs
@@ -3,6 +3,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut d = zune_jpeg::Decoder::new();
+    d.set_num_threads(1).unwrap();
 
     let _: Result<Vec<u8>, _> = d.decode_buffer(data);
 });
