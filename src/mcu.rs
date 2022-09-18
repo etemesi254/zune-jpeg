7efe0494585107875af47615321e1e22f4da20c1
caleb <etemesicaleb@gmail.com>
2022-09-18 16:17:21 +0300

    src/mcu:  Formatting improvements

--- src/mcu.rs
+++ src/mcu.rs
@@ -57,12 +57,13 @@ use std::cmp::min;
 use std::io::Cursor;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
+
 use crate::bitstream::BitStream;
 use crate::components::{ComponentID, SubSampRatios};
 use crate::errors::DecodeErrors;
 use crate::marker::Marker;
 use crate::worker::post_process;
-use crate::{Decoder, ColorSpace};
+use crate::{ColorSpace, Decoder};
 
 /// The size of a DC block for a MCU.
 
@@ -71,7 +72,7 @@ pub const DCT_BLOCK: usize = 64;
 impl Decoder
 {
     /// Check for existence of DC and AC Huffman Tables
-    pub (crate) fn check_tables(&self) -> Result<(), DecodeErrors>
+    pub(crate) fn check_tables(&self) -> Result<(), DecodeErrors>
     {
         // check that dc and AC tables exist outside the hot path
         for i in 0..self.input_colorspace.num_components()
@@ -129,6 +130,9 @@ impl Decoder
     ) -> Result<Vec<u8>, DecodeErrors>
     {
         self.check_component_dimensions()?;
+        // check dc and AC tables
+        self.check_tables()?;
+
         let  mut scoped_pools = scoped_threadpool::Pool::new(self.num_threads.unwrap_or(std::thread::available_parallelism().unwrap_or(NonZeroUsize::new(4).unwrap()).get()) as u32);
         info!("Created {} worker threads", scoped_pools.thread_count());
 
@@ -147,28 +151,20 @@ impl Decoder
                 // Values for horizontal samples end halfway the image and do not complete an MCU width.
                 // To make it complete we multiply width by 2 and divide mcu_height by 2
                 mcu_width = self.mcu_x * 2;
-
                 mcu_height = self.mcu_y / 2;
             } else if self.sub_sample_ratio == SubSampRatios::HV
             {
                 mcu_width = self.mcu_x;
-
                 mcu_height = self.mcu_y / 2;
-
                 bias = 2;
-
-
-                // V;
             } else {
                 mcu_width = self.mcu_x;
-
                 mcu_height = self.mcu_y;
             }
         } else {
             // For non-interleaved images( (1*1) subsampling)
             // number of MCU's are the widths (+7 to account for paddings) divided bu 8.
             mcu_width = ((self.info.width + 7) / 8) as usize;
-
             mcu_height = ((self.info.height + 7) / 8) as usize;
         }
 
@@ -180,9 +176,9 @@ impl Decoder
             This means some calculations will be wrong, so for that we explicitly reset params
             for such occurrences, warn and reset the image info to appear as if it were
             a non-sampled image to ensure decoding works
-            
             */
             warn!("Grayscale image with down-sampled component, resetting component details");
+
             mcu_width = ((self.info.width + 7) / 8) as usize;
             self.h_max = 1;
             self.output_colorspace= ColorSpace::GRAYSCALE;
@@ -194,52 +190,36 @@ impl Decoder
             mcu_height = ((self.info.height + 7) / 8) as usize;
             bias = 1;
         }
-        let mut stream = BitStream::new();
         // Size of our output image(width*height)
         let capacity = usize::from(self.info.width + 8) * usize::from(self.info.height + 8);
-
         let component_capacity = mcu_width * DCT_BLOCK;
         // Create an Arc of components to prevent cloning on every MCU width
         let global_component = Arc::new(self.components.clone());
-
         let is_hv = self.sub_sample_ratio == SubSampRatios::HV;
-
         // There are some images where we need to overallocate  especially for small buffers,
         // because the chunking calculation will do it wrongly,
         // this only applies to  small down-sampled images
         // See https://github.com/etemesi254/zune-jpeg/issues/11
         let extra_space = usize::from(self.interleaved) * 128 * usize::from(self.height()) * self.output_colorspace.num_components();
-
-        // Storage for decoded pixels
-        let mut global_channel = vec![0; (capacity * self.output_colorspace.num_components()) + extra_space];
-
         // things needed for post processing that we can remove out of the loop
         let input = self.input_colorspace;
-
         let output = self.output_colorspace;
-
         let idct_func = self.idct_func;
-
-
         let color_convert_16 = self.color_convert_16;
-
         let width = usize::from(self.width());
-
         let h_max = self.h_max;
-
         let v_max = self.v_max;
         // Halfway width size, used for vertical sub-sampling to write |Y2| in the right position.
         let width_stride = (component_capacity * self.components[0].vertical_sample * self.components[0].horizontal_sample * bias) >> 1;
-
         let hv_width_stride = width_stride >> 1;
-        // check dc and AC tables
-        self.check_tables()?;
 
+        let mut stream = BitStream::new();
+        // Storage for decoded pixels
+        let mut global_channel = vec![0; (capacity * self.output_colorspace.num_components()) + extra_space];
 
         // Split output into different blocks each containing enough space for an MCU width
         let mut chunks =
             global_channel.chunks_exact_mut(width * output.num_components() * 8 * h_max * v_max);
-
         let mut tmp = [0; DCT_BLOCK];
 
         // Argument for scoped threadpools, see file docs.
@@ -252,10 +232,10 @@ impl Decoder
                 // thread (that's how we're multi-threaded and thread safe).
 
                 let mut temporary = [vec![], vec![], vec![]];
+
                 for (pos, comp) in self.components.iter().enumerate()
                 {
                     // multiply capacity with sampling factor, it  should be 1*1 for un-sampled images
-
                     // Allocate only needed components.
                     if min(self.output_colorspace.num_components() - 1, pos) == pos
                     {
@@ -275,7 +255,6 @@ impl Decoder
                         for pos in 0..self.input_colorspace.num_components()
                         {
                             let component = &mut self.components[pos];
-
                             let dc_table = self.dc_huffman_tables[component.dc_huff_table & 3]
                                 .as_ref()
                                 .ok_or_else(|| {
@@ -284,7 +263,6 @@ impl Decoder
                                         component.component_id
                                     ))
                                 })?;
-
                             let ac_table = self.ac_huffman_tables[component.ac_huff_table & 3]
                                 .as_ref()
                                 .ok_or_else(|| {
@@ -310,33 +288,25 @@ impl Decoder
                                         // This is complex, don't even try to understand it. ~author
                                         let is_y =
                                             usize::from(component.component_id == ComponentID::Y);
-
                                         // This only affects 4:2:0 images.
                                         let y_offset = is_y
                                             * v
                                             * (hv_width_stride
                                             + (hv_width_stride * (component.vertical_sample - 1)));
-
                                         let another_stride =
                                             (width_stride * v_samp * usize::from(!is_hv))
                                                 + hv_width_stride * v_samp * usize::from(is_hv);
-
                                         let yet_another_stride = usize::from(is_hv)
                                             * (width_stride >> 2)
                                             * v
                                             * usize::from(component.component_id != ComponentID::Y);
-
                                         // offset calculator.
                                         let start = (j * 64 * component.horizontal_sample)
                                             + (h_samp * 64)
                                             + another_stride
                                             + y_offset
                                             + yet_another_stride;
-
-                                        // Get the location we will be writing to.
-
                                         // It will always be zero since it's initialized per MCU height.
-
                                         let tmp: &mut [i16; 64] = temporary.get_mut(pos).unwrap().get_mut(start..start + 64).unwrap().try_into().unwrap();
 
                                         stream.decode_mcu_block(reader, dc_table, ac_table, tmp, &mut component.dc_pred)?;
@@ -366,6 +336,7 @@ impl Decoder
                                 {
                                     break;
                                 }
+
                                 match m
                                 {
                                     // leave RST handling to the other routine when it's ready
@@ -380,11 +351,15 @@ impl Decoder
                 }
                 // Clone things, to make multithreading safe
                 let component = global_component.clone();
-
                 let next_chunk = chunks.next().unwrap();
 
                 scope.execute(move || {
-                    post_process(&mut temporary, &component,
+                    let mut coeffs :[&[i16];3]=[&[];3];
+
+                    temporary.iter().enumerate().for_each(|(pos,x)|{
+                        coeffs[pos] = x;
+                    });
+                    post_process(&coeffs, &component,
                                  idct_func, color_convert_16,
                                  input, output, next_chunk,
                                  width);
@@ -417,24 +392,24 @@ impl Decoder
             match marker
             {
                 Marker::RST(_) =>
-                    {
-                        // reset stream
-                        stream.reset();
-                        // Initialize dc predictions to zero for all components
-                        self.components.iter_mut().for_each(|x| x.dc_pred = 0);
-                        // Start iterating again. from position.
-                    }
+                {
+                    // reset stream
+                    stream.reset();
+                    // Initialize dc predictions to zero for all components
+                    self.components.iter_mut().for_each(|x| x.dc_pred = 0);
+                    // Start iterating again. from position.
+                }
                 Marker::EOI =>
-                    {
-                        // silent pass
-                    }
+                {
+                    // silent pass
+                }
                 _ =>
-                    {
-                        return Err(DecodeErrors::MCUError(format!(
-                            "Marker {:?} found in bitstream, possibly corrupt jpeg",
-                            marker
-                        )));
-                    }
+                {
+                    return Err(DecodeErrors::MCUError(format!(
+                        "Marker {:?} found in bitstream, possibly corrupt jpeg",
+                        marker
+                    )));
+                }
             }
         }
 
