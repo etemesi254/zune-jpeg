31d8ed81e85e18cb5022269d72578b2d3d5cac0f
caleb <etemesicaleb@gmail.com>
2022-09-18 16:18:47 +0300

    src/mcu_prog: Formatting improvements.

--- src/mcu_prog.rs
+++ src/mcu_prog.rs
@@ -32,14 +32,14 @@ use std::sync::Arc;
 
 use crate::bitstream::BitStream;
 use crate::components::{ComponentID, SubSampRatios};
+use crate::decoder::MAX_COMPONENTS;
 use crate::errors::DecodeErrors;
+use crate::errors::DecodeErrors::Format;
 use crate::headers::{parse_huffman, parse_sos};
 use crate::marker::Marker;
 use crate::misc::read_byte;
-use crate::worker::post_process_prog;
+use crate::worker::post_process;
 use crate::{ColorSpace, Decoder};
-use crate::decoder::MAX_COMPONENTS;
-use crate::errors::DecodeErrors::Format;
 
 impl Decoder
 {
@@ -52,29 +52,26 @@ impl Decoder
     ) -> Result<Vec<u8>, DecodeErrors>
     {
         self.check_component_dimensions()?;
+        let mcu_height;
+
         // memory location for decoded pixels for components
         let mut block = [vec![], vec![], vec![]];
-
         let mut mcu_width;
-
-        let mcu_height;
 
         if self.interleaved
         {
             mcu_width = self.mcu_x;
-
             mcu_height = self.mcu_y;
         } else {
             mcu_width = (self.info.width as usize + 7) / 8;
-
             mcu_height = (self.info.height as usize + 7) / 8;
         }
+
         mcu_width *= 64;
 
         for i in 0..self.input_colorspace.num_components()
         {
             let comp = &self.components[i];
-
             let len = mcu_width * comp.vertical_sample * comp.horizontal_sample * mcu_height;
 
             block[i] = vec![0; len];
@@ -110,7 +107,6 @@ impl Decoder
                         marker = get_marker(reader, &mut stream).ok_or_else(|| DecodeErrors::Format(format!("Marker missing where expected")))?;
 
                         stream.reset();
-
                         continue 'eoi;
                     }
                 _ =>
@@ -163,63 +159,50 @@ impl Decoder
         let y = &block[0];
         let cb = &block[1];
         let cr = &block[2];
-
         let extra_space = usize::from(self.interleaved) * 128 * usize::from(self.height()) * self.output_colorspace.num_components();
-
         let capacity = usize::from(self.info.width + 8) * usize::from(self.info.height + 8);
 
         let mut out_vector = vec![0_u8; capacity * self.output_colorspace.num_components() + extra_space];
 
         // Things we need for multithreading.
         let h_max = self.h_max;
-
         let v_max = self.v_max;
-
         let components = Arc::new(self.components.clone());
-
-        let mut pool = scoped_threadpool::Pool::new(self.num_threads.unwrap_or(std::thread::available_parallelism().unwrap_or(NonZeroUsize::new(4).unwrap()).get()) as u32);
-
         let input = self.input_colorspace;
-
         let output = self.output_colorspace;
-
         let idct_func = self.idct_func;
-
-
         let color_convert_16 = self.color_convert_16;
-
         let width = usize::from(self.width());
-
         // Divide the output into small blocks and send to threads/
         let chunks_size = width * self.output_colorspace.num_components() * 8 * h_max * v_max;
-
         let out_chunks = out_vector.chunks_exact_mut(chunks_size);
-
         // Chunk sizes. Each determine how many pixels go per thread.
         let y_chunk_size =
             mcu_width * self.components[0].vertical_sample * self.components[0].horizontal_sample * bias;
-        // Cb and Cr contains equal sub-sampling so don't calculate for them.
-
         // Divide into chunks
         let y_chunk = y.chunks_exact(y_chunk_size);
+
+        let mut pool = scoped_threadpool::Pool::new(self.num_threads
+            .unwrap_or(std::thread::available_parallelism().unwrap_or(NonZeroUsize::new(4).unwrap()).get()) as u32);
+
 
         if self.input_colorspace.num_components() == 3 {
             let cb_chunk_size =
                 mcu_width * self.components[1].vertical_sample * self.components[1].horizontal_sample * bias;
 
             let cb_chunk = cb.chunks_exact(cb_chunk_size);
-
             let cr_chunk = cr.chunks_exact(cb_chunk_size);
-
             // open threads.
             pool.scoped(|scope| {
-                for (((y, cb), cr), out) in
-                y_chunk.zip(cb_chunk).zip(cr_chunk).zip(out_chunks)
+                for (((y, cb), cr), out) in y_chunk
+                    .zip(cb_chunk)
+                    .zip(cr_chunk)
+                    .zip(out_chunks)
                 {
                     let component = components.clone();
 
                     scope.execute(move || {
-                        post_process_prog(&[y, cb, cr], &component, idct_func, color_convert_16,
+                        post_process(&[y, cb, cr], &component, idct_func, color_convert_16,
                                           input, output, out, width,
                         );
                     });
@@ -231,8 +214,9 @@ impl Decoder
                 for (y, out) in y_chunk.zip(out_chunks)
                 {
                     let component = components.clone();
+
                     scope.execute(move || {
-                        post_process_prog(&[y, &[], &[]], &component, idct_func, color_convert_16,
+                        post_process(&[y, &[], &[]], &component, idct_func, color_convert_16,
                                           input, output, out, width,
                         );
                     });
@@ -260,7 +244,6 @@ impl Decoder
         stream.reset();
         self.components.iter_mut().for_each(|x| x.dc_pred = 0);
 
-
         if usize::from(self.num_scans) > self.input_colorspace.num_components() {
             return Err(Format(format!("Number of scans {} cannot be greater than number of components, {}", self.num_scans, self.input_colorspace.num_components())));
         }
@@ -277,28 +260,26 @@ impl Decoder
             // non interleaved data, process one block at a time in trivial scanline order
 
             let k = self.z_order[0];
+
             if k >= self.components.len() {
                 return Err(DecodeErrors::Format(format!("Cannot find component {}, corrupt image", k)));
             }
+
             let (mcu_width, mcu_height);
             // For Y channel  or non interleaved scans , mcu's is the image dimensions divided
             // by 8
             if self.components[k].component_id == ComponentID::Y || !self.interleaved
             {
                 mcu_width = ((self.info.width + 7) / 8) as usize;
-
                 mcu_height = ((self.info.height + 7) / 8) as usize;
             } else {
                 // For other channels, in an interleaved mcu, number of MCU's
                 // are determined by some weird maths done in headers.rs->parse_sos()
                 mcu_width = self.mcu_x;
-
                 mcu_height = self.mcu_y;
             }
             let mut i = 0;
-
             let mut j = 0;
-
 
             while i < mcu_height
             {
@@ -309,6 +290,7 @@ impl Decoder
                     if i >= mcu_height {
                         break;
                     }
+
                     let data: &mut [i16; 64] = buffer.get_mut(k)
                         .unwrap().get_mut(start..start + 64)
                         .unwrap().try_into().unwrap();
@@ -320,8 +302,8 @@ impl Decoder
                             .ok_or_else(|| DecodeErrors::Format(format!("No huffman table for component:{}", pos)))?
                             .as_ref()
                             .ok_or_else(|| DecodeErrors::Format(format!("Huffman table at index  {} not initialized", pos)))?;
-
                         let dc_pred = &mut self.components[k].dc_pred;
+
                         if self.succ_high == 0
                         {
                             // first scan for this mcu
@@ -330,9 +312,9 @@ impl Decoder
                             // refining scans for this MCU
                             stream.decode_prog_dc_refine(reader, &mut data[0])?;
                         }
+
                     } else {
                         let pos = self.components[k].ac_huff_table;
-
                         let ac_table = self.ac_huffman_tables.get(pos)
                             .ok_or_else(|| DecodeErrors::Format(format!("No huffman table for component:{}", pos)))?
                             .as_ref()
@@ -354,9 +336,7 @@ impl Decoder
                                 // as big as 10,000
 
                                 i += (j + stream.eob_run as usize - 1) / mcu_width;
-
                                 j = (j + stream.eob_run as usize - 1) % mcu_width;
-
                                 stream.eob_run = 0;
                             } else {
                                 stream.decode_mcu_ac_first(reader, ac_table, data)?;
@@ -367,7 +347,6 @@ impl Decoder
                         }
                     }
                     j += 1;
-
                     self.todo -= 1;
 
                     if self.todo == 0
@@ -397,28 +376,24 @@ impl Decoder
                     for k in 0..self.num_scans
                     {
                         let n = self.z_order[k as usize];
+
                         if n >= self.components.len() {
                             return Err(DecodeErrors::Format(format!("Cannot find component {}, corrupt image", n)));
                         }
 
                         let component = &mut self.components[n];
-
                         let huff_table = self.dc_huffman_tables.get(component.dc_huff_table)
                             .ok_or_else(|| DecodeErrors::Format(format!("No huffman table for component:{}", component.dc_huff_table)))?
                             .as_ref()
-                            .ok_or_else(|| DecodeErrors::Format(format!("Huffman table at index  {} not initialized", component.dc_huff_table)))?
-                            ;
+                            .ok_or_else(|| DecodeErrors::Format(format!("Huffman table at index  {} not initialized", component.dc_huff_table)))?;
 
                         for v_samp in 0..component.vertical_sample
                         {
                             for h_samp in 0..component.horizontal_sample
                             {
                                 let x2 = j * component.horizontal_sample + h_samp;
-
                                 let y2 = i * component.vertical_sample + v_samp;
-
                                 let position = 64 * (x2 + y2 * component.width_stride / 8);
-
                                 // data will contain the position for this coefficient in our array.
                                 let data = &mut buffer[n as usize][position];
 
@@ -488,4 +463,3 @@ fn get_marker(reader: &mut Cursor<Vec<u8>>, stream: &mut BitStream) -> Option<Ma
         }
     }
 }
-
